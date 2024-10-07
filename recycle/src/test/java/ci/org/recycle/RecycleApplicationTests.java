06b5e4077dabf0601b13a25f8ed68b77a828e2f4
gerard-ks <siaka.kouakou.gerard@gmail.com>
2024-10-07 16:48:21 +0000

    second commit

---
+++ recycle/src/test/java/ci/org/recycle/RecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.org.recycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
